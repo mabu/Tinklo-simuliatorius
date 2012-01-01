d5cbab0cea2777341847cc8fbd77e5d4d4f56a1d
Martynas Budriūnas <martynas@budrinas.lt>
2012-01-02 01:08:23 +0200

    Prikabintas nekaltai pamirštas types.h

---
+++ types.h
@@ -0,0 +1,13 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#define BROADCAST_MAC 0xffffffffffff
+
+using namespace std;
+
+typedef unsigned       IpAddress;
+typedef long long      MacAddress;
+typedef unsigned char  Byte;
+typedef unsigned short FrameLength;
+
+#endif
